1efc5d7134582d72bd7cd204e8a65acccbc8db63
SushiAbuse <65246869+SushiAbuse@users.noreply.github.com>
2022-03-29 21:30:55 -0500

    Update mavalloc.c
    mavalloc_alloc edited

--- mavalloc.c
+++ mavalloc.c
@@ -14,6 +14,14 @@ int mavalloc_init( size_t size, enum ALGORITHM algorithm )
  
   size_t requested_size = ALIGN4( size );
   head = (Node *) malloc(requested_size * sizeof(Node)); 
+  head->type = HOLE;
+  head->size = requested_size;
+  head->prev = NULL;
+  head->next = NULL;
+
+  //Write algorithmn here: Based on user
+  //First Fit, Best Fit, Worst Fit, Next Fit
+  //After we allocate the memory and place it in its location
 
   //P: If the allocation fails or the size is less than 0 the function returns -1
   if((head == NULL) || (requested_size < 0))
@@ -30,10 +38,34 @@ void mavalloc_destroy( )
   return;
 }
 
+//J: Allocates size bytes from preallocated memory arena
+//J: [Hole/Process] [Start Number] [Length] [Prev] [Next]
 void * mavalloc_alloc( size_t size )
 {
-  // only return NULL on failure
-  return NULL;
+
+  Node *ptr, *temp;
+  //Finding end of linked list
+  temp = head;
+  while(temp->next != NULL)
+  {
+    temp = temp->next;
+  }
+
+  //Taking memory from preallocated memory arena
+  if(head->size <= 0 || (head->size-size) < 0)
+  {
+    //J: There is no more preallocated memory arena or enough
+    return NULL;
+  }
+  else
+  {
+    ptr->size = head->size-size;
+    temp->next = ptr;
+    ptr->prev = temp;
+    ptr->next = NULL;
+  }
+
+  return ptr;
 }
 
 void mavalloc_free( void * ptr )
@@ -41,6 +73,7 @@ void mavalloc_free( void * ptr )
   return;
 }
 
+//J: Function returns the number of nodes in the memory area
 int mavalloc_size( )
 {
   int number_of_nodes = 0;
